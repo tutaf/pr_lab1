c48e206e26f1f31d080135eae2e8c0aac45d3954
tutaf <andrei.cernisov@isa.utm.md>
2024-10-08 12:48:41 +0300

    map price to eur, filter products by price, reduce filtered products to sum of their prices

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from functools import reduce
+from datetime import datetime
+
+
+def convert_price_to_eur(mdl):
+    conversion_rate = 19.242
+    return round(mdl / conversion_rate, 2)
+
 
 url = "https://maximum.md/ro/electrocasnice-mari/aspiratoare/aparate-de-spalat-pentru-auto/"
 
@@ -8,6 +16,8 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
 response = requests.get(url, headers=headers)
+
+products_list = []
 
 if response.status_code == 200:
     # parse html using bs4 parser
@@ -37,9 +47,36 @@ if response.status_code == 200:
         link_tag = name_tag.find('a') if name_tag else None
         product_link = f"https://maximum.md{link_tag['href']}" if link_tag else None
 
-        # validate data before printing
+        # validate data before appending
         if name and price is not None and product_link:
-            print(f"Product Name: {name}, Price: {price}, Link: {product_link}")
+            products_list.append({
+                "name": name,
+                "price_mdl": price,
+                "link": product_link
+            })
 
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
+# convert mdl to eur
+mapped_products = list(map(lambda p: {
+    **p,
+    "price_eur": convert_price_to_eur(p["price_mdl"])
+}, products_list))
+
+# filter by price
+min_price = 1000
+max_price = 2000
+filtered_products = list(filter(lambda p: min_price <= p["price_mdl"] <= max_price, mapped_products))
+
+# calculate sum using reduce
+total_sum_mdl = reduce(lambda acc, p: acc + p["price_mdl"], filtered_products, 0)
+
+utc_timestamp = datetime.utcnow().isoformat()
+result = {
+    "total_sum_mdl": total_sum_mdl,
+    "timestamp_utc": utc_timestamp,
+    "filtered_products": filtered_products,
+}
+
+print(result)
